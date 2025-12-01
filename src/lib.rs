54669992f7cd6bfa29eca06c9dfe2a2b0955d9ec
OlivierNeyret <olivier@neyret.net>
2025-12-01 22:36:56 +0100

    Add day 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,66 @@
-pub fn day1(input: String) -> u32 {
-    0
+use std::{fs, num::ParseIntError};
+
+#[derive(PartialEq, Debug)]
+pub enum Day1Error {
+    IllformedOffset(ParseIntError),
+    IllformedDirection,
+    FileReadingError,
+}
+
+enum RotationDirection {
+    Left,
+    Right,
+}
+
+fn rotate(current: i32, direction: RotationDirection, offset: i32) -> i32 {
+    let mut new_position = current;
+    match direction {
+        RotationDirection::Left => {
+            new_position -= offset;
+        }
+        RotationDirection::Right => {
+            new_position += offset;
+        }
+    }
+    new_position.rem_euclid(100)
+}
+
+fn parse_line(line: &str) -> Result<(RotationDirection, i32), Day1Error> {
+    let (direction_str, offset_str) = line.split_at(1);
+    let offset = match offset_str.parse::<i32>() {
+        Ok(value) => value,
+        Err(e) => return Err(Day1Error::IllformedOffset(e)),
+    };
+
+    let direction = match direction_str {
+        "L" => RotationDirection::Left,
+        "R" => RotationDirection::Right,
+        _ => {return Err(Day1Error::IllformedDirection) },
+    };
+    Ok((direction, offset))
+}
+
+fn parse_file(input: &str) -> Result<u32, Day1Error> {
+    let mut count_zero: u32 = 0;
+    let mut current_pointed_value: i32 = 50;
+    for line in input.lines() {
+        let (direction, offset) = parse_line(line)?;
+
+        current_pointed_value = rotate(current_pointed_value, direction, offset);
+
+        if current_pointed_value == 0 {
+            count_zero += 1;
+        }
+    }
+    Ok(count_zero)
+}
+
+pub fn day1_file(path: &str) ->  Result<u32, Day1Error> {
+    let file_content = match fs::read_to_string(path) {
+        Ok(content) => content,
+        Err(_) => { return Err(Day1Error::FileReadingError) },
+    };
+    parse_file(file_content.as_str())
 }
 
 #[cfg(test)]
@@ -9,21 +70,13 @@ mod tests {
     #[test]
     fn day1_example() {
         // Given
-        let input = String::from("L68
-            L30
-            R48
-            L5
-            R60
-            L55
-            L1
-            L99
-            R14
-            L82");
+        let file_path = "inputs/day1_example.txt";
 
         // When
-        let result = day1(input);
+        let result = day1_file(file_path);
 
         // Then
-        assert_eq!(result, 3);
+        println!("{:?}", result);
+        assert_eq!(result, Ok(3));
     }
 }
