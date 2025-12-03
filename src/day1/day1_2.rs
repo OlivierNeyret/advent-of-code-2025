ba599f5e359e117b0a08cb7a0fdbdf36c55e4ee8
OlivierNeyret <olivier@neyret.net>
2025-12-03 21:14:45 +0100

    Solve day 1 part 2

--- src/day1/day1_2.rs
+++ src/day1/day1_2.rs
@@ -1,19 +1,32 @@
 use crate::day1::common::{Day1Error, RotationDirection};
 
-fn rotate(current: i32, direction: RotationDirection, offset: i32) -> i32 {
-    let mut new_position = current;
-    match direction {
-        RotationDirection::Left => {
-            new_position -= offset;
-        }
-        RotationDirection::Right => {
-            new_position += offset;
-        }
-    }
-    new_position.rem_euclid(100)
+struct Rotation {
+    pub direction: RotationDirection,
+    pub offset: i32,
 }
 
-fn parse_line(line: &str) -> Result<(RotationDirection, i32), Day1Error> {
+struct RotationResult {
+    pub position: i32,
+    pub nb_zero_pointed: u32,
+}
+
+fn rotate(current: i32, rotation: Rotation) -> RotationResult {
+    let mut new_position = current;
+    let mut nb_zeros: u32 = 0;
+    for _i in 0..rotation.offset {
+        new_position += match rotation.direction {
+            RotationDirection::Left => -1,
+            RotationDirection::Right => 1,
+        };
+        new_position = new_position.rem_euclid(100);
+        if new_position == 0 {
+            nb_zeros +=1;
+        }
+    }
+    RotationResult { position: new_position, nb_zero_pointed: nb_zeros }
+}
+
+fn parse_line(line: &str) -> Result<Rotation, Day1Error> {
     let (direction_str, offset_str) = line.split_at(1);
     let offset = match offset_str.parse::<i32>() {
         Ok(value) => value,
@@ -25,20 +38,18 @@ fn parse_line(line: &str) -> Result<(RotationDirection, i32), Day1Error> {
         "R" => RotationDirection::Right,
         _ => {return Err(Day1Error::IllformedDirection) },
     };
-    Ok((direction, offset))
+    Ok(Rotation { direction, offset })
 }
 
 pub fn parse_file(input: &str) -> Result<u32, Day1Error> {
     let mut count_zero: u32 = 0;
     let mut current_pointed_value: i32 = 50;
     for line in input.lines() {
-        let (direction, offset) = parse_line(line)?;
+        let rotation = parse_line(line)?;
 
-        current_pointed_value = rotate(current_pointed_value, direction, offset);
-
-        if current_pointed_value == 0 {
-            count_zero += 1;
-        }
+        let rotation_result = rotate(current_pointed_value, rotation);
+        current_pointed_value = rotation_result.position;
+        count_zero += rotation_result.nb_zero_pointed;
     }
     Ok(count_zero)
 }
