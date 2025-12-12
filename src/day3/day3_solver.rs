01e927f2cf93725b9a31b35e305ceeb7c9cd2bcf
OlivierNeyret <olivier@neyret.net>
2025-12-12 15:56:54 +0100

    Solve day 3 part 1

--- src/day3/day3_solver.rs
+++ src/day3/day3_solver.rs
@@ -7,6 +7,7 @@ use std::{fs, num::ParseIntError};
 pub enum Day3Error {
     FileReadingError,
     IllformedBank(ParseIntError),
+    EmptyBank,
 }
 
 impl From<ParseIntError> for Day3Error {
@@ -15,27 +16,45 @@ impl From<ParseIntError> for Day3Error {
     }
 }
 
-fn parse_bank(bank: &str, _part: &DayPart) -> Result<u64, Day3Error> {
-    //TODO: isolate this in a function parse_bank and rename this function calculate_bank_jolts
-    let all_jolts = bank.chars()
+fn parse_bank(bank: &str, _part: &DayPart) -> Result<Vec<u64>, Day3Error> {
+    bank.chars()
         .collect::<Vec<char>>()
         .into_iter()
         .chunks(1)
         .into_iter()
         .map(|chunk| {
             let chunk_str: String = chunk.collect();
-            chunk_str
-                .parse::<u64>()
-                .map_err(|e| Day3Error::IllformedBank(e))
+            chunk_str.parse::<u64>().map_err(Day3Error::IllformedBank)
         })
-        .collect();
+        .collect()
+}
 
-    //TODO: iterate over all_jolts and find the first max value.
-    // Then find the next max value, starting from previous max
+fn max_jolts_from_bank(bank: Vec<u64>, _part: &DayPart) -> Result<u64, Day3Error> {
+    let mut first_max = 0;
+    let mut second_max = 0;
+    let nb_batteries = bank.len();
+    for (idx, jolt) in bank.into_iter().enumerate() {
+        if jolt > second_max {
+            second_max = jolt;
+            if second_max > first_max && idx < nb_batteries - 1 {
+                first_max = second_max;
+                second_max = 0;
+            }
+        }
+    }
+
+    Ok(first_max * 10 + second_max)
+}
+
+fn calculate_bank_jolts(bank: &str, _part: &DayPart) -> Result<u64, Day3Error> {
+    max_jolts_from_bank(parse_bank(bank, _part)?, _part)
 }
 
 fn parse_file(input: &str, part: DayPart) -> Result<u64, Day3Error> {
-    input.lines().map(|bank| parse_bank(bank, &part)).sum()
+    input
+        .lines()
+        .map(|bank| calculate_bank_jolts(bank, &part))
+        .sum()
 }
 
 pub fn solve_day3(path: &str, part: DayPart) -> Result<u64, Day3Error> {
@@ -53,7 +72,8 @@ mod tests {
     #[test]
     fn day3_1_example() {
         // Given
-        let file_path = "inputs/day3_example.txt";
+        let file_path = "inputs/day3.txt";
+        // let file_path = "inputs/day3_example.txt";
 
         // When
         let result = solve_day3(file_path, DayPart::Part1);
