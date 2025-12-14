5bdca0997891819368d7e1ae2f1b551c59438219
OlivierNeyret <olivier@neyret.net>
2025-12-14 18:03:46 +0100

    Solve day 5 part 2

--- src/day5/day5_solver.rs
+++ src/day5/day5_solver.rs
@@ -1,6 +1,6 @@
 use super::DayPart;
 
-use std::{fs, num::ParseIntError};
+use std::{fs, num::ParseIntError, vec};
 
 #[derive(PartialEq, Debug)]
 pub enum Day5Error {
@@ -15,14 +15,26 @@ impl From<ParseIntError> for Day5Error {
     }
 }
 
+#[derive(Clone, Copy)]
 struct Range {
-    begin: u64,
-    end: u64,
+    begin: usize,
+    end: usize,
 }
 
 impl Range {
-    fn is_in_range(&self, id: u64) -> bool {
+    fn contains(&self, id: usize) -> bool {
         id >= self.begin && id <= self.end
+    }
+
+    fn is_overlapping(&self, other: &Range) -> bool {
+        self.begin <= other.end && other.begin <= self.end
+    }
+
+    fn union(&self, other: &Range) -> Range {
+        Range {
+            begin: self.begin.min(other.begin),
+            end: self.end.max(other.end),
+        }
     }
 }
 
@@ -31,13 +43,55 @@ struct FreshIds {
 }
 
 impl FreshIds {
-    fn is_fresh(&self, id: u64) -> bool {
-        self.ids.iter().any(|range| range.is_in_range(id))
+    fn new(mut ranges: Vec<Range>) -> FreshIds {
+        ranges.sort_by(|a, b| a.begin.cmp(&b.begin));
+
+        let mut i = 0;
+        while i < ranges.len() - 1 {
+            if ranges[i].is_overlapping(&ranges[i + 1]) {
+                ranges[i] = ranges[i].union(&ranges[i + 1]);
+                ranges.remove(i + 1);
+            } else {
+                i += 1;
+            }
+        }
+
+        FreshIds { ids: ranges }
+        // if ids.is_empty() {
+        //     return FreshIds { ids: vec![] }
+        // }
+
+        // let mut squashed_ranges = vec![];
+        // squashed_ranges.push(ids[0]);
+
+        // for range in ids.iter().skip(1) {
+        //     // let mut range_container: Option<&Range> = None;
+        //     for squashed_range in squashed_ranges.iter_mut() {
+        //         if range.is_overlapping(squashed_range) {
+        //             *squashed_range = Range {
+        //                 begin: range.begin.min(squashed_range.begin),
+        //                 end: range.end.max(squashed_range.end),
+        //             };
+        //         }
+        //     }
+        // }
+
+        // FreshIds { ids:squashed_ranges }
+    }
+
+    fn is_fresh(&self, id: usize) -> bool {
+        self.ids.iter().any(|range| range.contains(id))
+    }
+
+    fn count(&self) -> usize {
+        self.ids
+            .iter()
+            .fold(0, |c, range| c + range.end - range.begin + 1)
     }
 }
 
 struct AvailableIds {
-    ids: Vec<u64>,
+    ids: Vec<usize>,
 }
 
 fn parse_file(content: &str) -> Result<(FreshIds, AvailableIds), Day5Error> {
@@ -54,11 +108,11 @@ fn parse_file(content: &str) -> Result<(FreshIds, AvailableIds), Day5Error> {
             let range_begin = range_it
                 .next()
                 .ok_or(Day5Error::IllformedRange)?
-                .parse::<u64>()?;
+                .parse::<usize>()?;
             let range_end = range_it
                 .next()
                 .ok_or(Day5Error::IllformedRange)?
-                .parse::<u64>()?;
+                .parse::<usize>()?;
             fresh_ids.push(Range {
                 begin: range_begin,
                 end: range_end,
@@ -68,22 +122,25 @@ fn parse_file(content: &str) -> Result<(FreshIds, AvailableIds), Day5Error> {
         }
     }
     Ok((
-        FreshIds { ids: fresh_ids },
+        FreshIds::new(fresh_ids),
         AvailableIds { ids: available_ids },
     ))
 }
 
-pub fn solve_day5(path: &str, _part: DayPart) -> Result<usize, Day5Error> {
+pub fn solve_day5(path: &str, part: DayPart) -> Result<usize, Day5Error> {
     let file_content = match fs::read_to_string(path) {
         Ok(content) => content,
         Err(_) => return Err(Day5Error::FileReadingError),
     };
     let (fresh_ids, available_ids) = parse_file(file_content.as_str())?;
-    Ok(available_ids
-        .ids
-        .into_iter()
-        .filter(|id| fresh_ids.is_fresh(*id))
-        .count())
+    match part {
+        DayPart::Part1 => Ok(available_ids
+            .ids
+            .into_iter()
+            .filter(|id| fresh_ids.is_fresh(*id))
+            .count()),
+        DayPart::Part2 => Ok(fresh_ids.count()),
+    }
 }
 
 #[cfg(test)]
@@ -100,5 +157,17 @@ mod tests {
 
         // Then
         assert_eq!(result, Ok(3));
+    }
+
+    #[test]
+    fn day5_2_example() {
+        // Given
+        let file_path = "inputs/day5_example.txt";
+
+        // When
+        let result = solve_day5(file_path, DayPart::Part2);
+
+        // Then
+        assert_eq!(result, Ok(14));
     }
 }
